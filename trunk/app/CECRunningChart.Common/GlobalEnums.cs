59111e5eda33bea316ef1cf6e49abc2ba236b45e
Saliya Ranawaka <saliya@deesea.com>
2012-07-31 21:34:30 +0530

    Added common project

---
+++ trunk/app/CECRunningChart.Common/GlobalEnums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CECRunningChart.Common
+{
+    public enum UserRoles
+    {
+        Role1 = 0,
+        Role2,
+        Role3,
+        Role4
+    }
+}
