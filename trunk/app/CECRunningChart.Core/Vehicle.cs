ba43d88ac005aa43ea756d4d518bc4184eebbadf
Saliya Ranawaka <saliya@deesea.com>
2012-08-01 10:25:38 +0530

    new vehicle properties

--- trunk/app/CECRunningChart.Core/Vehicle.cs
+++ trunk/app/CECRunningChart.Core/Vehicle.cs
@@ -7,15 +7,61 @@ namespace CECRunningChart.Core
         [XmlElementAttribute("Id")]
         public int Id { get; set; }
 
+        /// <summary>
+        /// Vehicle registred number
+        /// </summary>
         [XmlElementAttribute("VehicleNumber")]
         public string VehicleNumber { get; set; }
 
+        /// <summary>
+        /// Vehicle category id
+        /// </summary>
         [XmlElementAttribute("VehicleType")]
-        public string VehicleType { get; set; }
+        public string VehicleType { get; set; } // TODO: rename to VehicleTypeId
 
-        public string FuelUsage { get; set; } //rate
+        /// <summary>
+        /// Vehicle category name
+        /// </summary>
+        public string VehicleTypeName { get; set; }
 
+        /// <summary>
+        /// Current rate (Km/L or Hrs/L) of the vehicle/machine
+        /// </summary>
+        public string FuelRate { get; set; }
+
+        public string ActualFuelRate { get; set; } //TODO: Is this calculated or?
+
+        /// <summary>
+        /// Fuel type petrol or desl- TODO
+        /// </summary>
         public string FuelType { get; set; }
+
+        public string LubricantType { get; set; }
+
+        /// <summary>
+        /// Is the vehicle/machine hired by CEC
+        /// </summary>
+        public bool IsHiredVehicle { get; set; }
+
+        /// <summary>
+        /// If hired, hire rate in Rs
+        /// </summary>
+        public decimal HireRate { get; set; }
+
+        /// <summary>
+        /// Vehicle owner's name of hired vehicles
+        /// </summary>
+        public string OwnerName { get; set; }
+
+        /// <summary>
+        /// Allocated driver for vehicle/machine
+        /// </summary>
+        public string DriverOperatorName { get; set; }
+
+        /// <summary>
+        /// Unique code supplied by the company for each vehicle/machine
+        /// </summary>
+        public string CompanyCode { get; set; }
 
         [XmlElementAttribute("Description")]
         public string Description { get; set; }
